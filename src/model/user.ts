bc97dfb644a55bd68c844ed2792228edf38f6507
george franklin <giorgio1023@gmail.com>
2023-01-27 18:25:51 +0100

    First impl with axios

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,7 +1,68 @@
+export interface IUserResponse {
+  limit: number;
+  skip: 0;
+  total: number;
+  users: IUser[];
+}
+
 export interface IUser {
   id: number;
-  name: string;
-  job: string;
-  image: string;
+  firstName: string;
+  lastName: string;
+  maidenName: string;
   age: number;
+  gender: string;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: {
+    color: string;
+    type: string;
+  };
+  domain: string;
+  ip: string;
+  address: {
+    address: string;
+    city: string;
+    coordinates: {
+      lat: number;
+      lng: number;
+    };
+    postalCode: string;
+    state: string;
+  };
+  macAddress: string;
+  university: string;
+  bank: {
+    cardExpire: string;
+    cardNumber: string;
+    cardType: string;
+    currency: string;
+    iban: string;
+  };
+  company: {
+    address: {
+      address: string;
+      city: string;
+      coordinates: {
+        lat: number;
+        lng: number;
+      };
+      postalCode: string;
+      state: string;
+    };
+    department: string;
+    name: string;
+    title: string;
+  };
+  ein: string;
+  ssn: string;
+  userAgent: string;
 }
